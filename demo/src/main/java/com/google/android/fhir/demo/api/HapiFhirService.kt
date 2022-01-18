b6ab3671eaf819a3e6c8ed8e13be884e0290c062
Timon Grob <timongrob@gmail.com>
2022-01-18 23:52:43 +0100

    updated to query local FHIR server

--- demo/src/main/java/com/google/android/fhir/demo/api/HapiFhirService.kt
+++ demo/src/main/java/com/google/android/fhir/demo/api/HapiFhirService.kt
@@ -53,7 +53,7 @@ interface HapiFhirService {
   suspend fun deleteResource(@Path("type") type: String, @Path("id") id: String): OperationOutcome
 
   companion object {
-    const val BASE_URL = "https://hapi.fhir.org/baseR4/"
+    const val BASE_URL = "http://192.168.2.210:8400/fhir/"
 
     fun create(parser: IParser): HapiFhirService {
       val logger = HttpLoggingInterceptor()
